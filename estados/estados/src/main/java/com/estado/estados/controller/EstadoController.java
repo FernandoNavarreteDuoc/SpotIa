a77667d85ed7d8c0f099e5a4215fc9b171da9e68
FernandoNavarreteDuoc <fer.navarretea@duocuc.cl>
2026-06-03 18:33:45 -0400

    Se transcriben datos de estado

---
+++ estados/estados/src/main/java/com/estado/estados/controller/EstadoController.java
@@ -0,0 +1,72 @@
+package com.estado.estados.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estado.estados.DTO.EstadoDTO;
+import com.estado.estados.model.Estado;
+import com.estado.estados.service.EstadoService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/estados")
+public class EstadoController {
+
+    private final EstadoService service;
+
+    public EstadoController(EstadoService service) {
+
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Estado>> listar() {
+
+        return ResponseEntity.ok(service.listar());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Estado> buscar(
+            @PathVariable Integer id) {
+
+        return ResponseEntity.ok(service.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Estado> guardar(
+            @Valid @RequestBody EstadoDTO dto) {
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(service.guardar(dto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Estado> actualizar(
+            @PathVariable Integer id,
+            @Valid @RequestBody EstadoDTO dto) {
+
+        return ResponseEntity.ok(
+                service.actualizar(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(
+            @PathVariable Integer id) {
+
+        service.eliminar(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ estados/estados/src/main/java/com/estado/estados/model/Estado.java
@@ -0,0 +1,47 @@
+package com.estado.estados.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "estados")
+public class Estado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idEstado;
+
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @Size(min = 3, max = 50, message = "El nombre debe contener entre 3 y 50 caracteres")
+    @Column(unique = false, length = 50, nullable = false)
+    private String nombre;
+
+    @NotBlank(message = "La descripción no puede estar vacía")
+    @Size(min = 5, max = 100, message = "La descripción debe contener entre 5 y 100 caracteres")
+    @Column(unique = false, length = 100, nullable = false)
+    private String descripcion;
+
+    @OneToMany(mappedBy = "estado")
+    @ToString.Exclude
+    @JsonIgnore
+    private List<Reservas> reservas;
+
+}
