d9be0edd96883e13d253d7a8f4e1b2a3151cf8d1
Martín Maulén <mart.maulen@duocuc.cl>
2026-06-05 17:42:04 -0400

    local

---
+++ locales/locales/src/main/java/com/local/locales/DTO/localDTO.java
@@ -0,0 +1,15 @@
+package com.local.locales.DTO;
+
+import lombok.Data;
+
+@Data
+public class localDTO {
+
+    private Integer idLocal;
+    private String nombre;
+    private String descripcion;
+    private Integer capacidad;
+    private Integer idTipoLocal;
+    private Integer idUbicacion;
+
+}
---
+++ locales/locales/src/main/java/com/local/locales/repository/LocalRepository.java
@@ -0,0 +1,21 @@
+package com.local.locales.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.PaginaSpotIa.model.Local;
+
+@Repository
+public interface LocalRepository
+        extends JpaRepository<Local, Integer> {
+
+    @Query("SELECT l FROM Local l WHERE l.capacidad >= :capacidad")
+    List<Local> buscarLocalesGrandes(
+            @Param("capacidad") Integer capacidad
+    );
+
+}
