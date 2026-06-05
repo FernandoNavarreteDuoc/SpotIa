7bb315bf4c79a8889520e1de62690f476e8313fa
FernandoNavarreteDuoc <fer.navarretea@duocuc.cl>
2026-06-05 18:05:38 -0400

    corrección de errores

--- clientes/clientes/src/main/java/com/cliente/clientes/model/Cliente.java
+++ clientes/clientes/src/main/java/com/cliente/clientes/model/Cliente.java
@@ -15,13 +15,10 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @ToString
-@Getter
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
