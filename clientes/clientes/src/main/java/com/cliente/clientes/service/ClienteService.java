e51d275197403b5e700aa5c4397f3c679c8ea301
maxiscript777 <max.amestica@duocuc.cl>
2026-06-12 18:14:49 -0400

    cliente

--- clientes/clientes/src/main/java/com/cliente/clientes/service/ClienteService.java
+++ clientes/clientes/src/main/java/com/cliente/clientes/service/ClienteService.java
@@ -1,6 +1,5 @@
 package com.cliente.clientes.service;
 
-import com.cliente.clientes.controller.ClienteController;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -21,64 +20,72 @@ import reactor.core.publisher.Mono;
 public class ClienteService {
 
     @Autowired
-    private ClienteRepository clienteRepository;
+    private ClienteRepository repository;
 
     @Autowired
     private WebClient.Builder webClientBuilder;
 
-    private final ClienteController clienteController;
-
-    private final ClienteRepository repository;
-
     private static final Logger logger =
             LoggerFactory.getLogger(ClienteService.class);
 
-    public ClienteService(ClienteRepository repository, ClienteController clienteController) {
-        this.repository = repository;
-        this.clienteController = clienteController;
-    }
-
     public List<Cliente> listar() {
+
         logger.info("Listando clientes");
+
         return repository.findAll();
     }
 
     public Cliente buscarPorRut(String rut) {
+
         logger.info("Buscando cliente");
+
         return repository.findById(rut)
-                .orElseThrow(() -> new RuntimeException("Cliente no encontrado"));
+                .orElseThrow(() ->
+                        new RuntimeException("Cliente no encontrado"));
     }
 
     public Cliente guardar(ClienteDTO dto) {
+
         logger.info("Guardando cliente");
+
         Cliente cliente = new Cliente();
+
         cliente.setRut(dto.getRut());
         cliente.setNombre(dto.getNombre());
         cliente.setDireccion(dto.getDireccion());
         cliente.setTelefono(dto.getTelefono());
         cliente.setCorreo(dto.getCorreo());
         cliente.setContraseña(dto.getContraseña());
+
         return repository.save(cliente);
     }
 
     public Cliente actualizar(String rut, ClienteDTO dto) {
+
         logger.info("Actualizando cliente");
+
         Cliente cliente = buscarPorRut(rut);
+
         cliente.setNombre(dto.getNombre());
         cliente.setDireccion(dto.getDireccion());
         cliente.setTelefono(dto.getTelefono());
         cliente.setCorreo(dto.getCorreo());
         cliente.setContraseña(dto.getContraseña());
+
         return repository.save(cliente);
     }
 
     public void eliminar(String rut) {
+
         logger.info("Eliminando cliente");
+
         repository.deleteById(rut);
     }
 
-    private ClienteDTO convertirADTO(Cliente cliente){
+    public ClienteDTO convertirADTO(Cliente cliente) {
+
         ClienteDTO dto = new ClienteDTO();
+
         dto.setRut(cliente.getRut());
         dto.setNombre(cliente.getNombre());
         dto.setContraseña(cliente.getContraseña());
@@ -86,19 +93,27 @@ public class ClienteService {
         dto.setTelefono(cliente.getTelefono());
         dto.setDireccion(cliente.getDireccion());
 
-        try{
-            ReservaExternaDTO reservaRecuperada = webClientBuilder.build()
-                .get()
-                .uri("http://reservas/api/v1/reservas/buscar-por-cliente/" + cliente.getRut())
-                .retrieve()
-                .onStatus(HttpStatusCode::is4xxClientError, response -> Mono.empty()) // importante
-                .bodyToMono(ReservaExternaDTO.class)
-                .block();
+        try {
+
+            ReservaExternaDTO reservaRecuperada =
+                    webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8080/api/v1/reservas/buscar-por-cliente/" + cliente.getRut())
+                    .retrieve()
+                    .onStatus(
+                            HttpStatusCode::is4xxClientError,
+                            response -> Mono.empty()
+                    )
+                    .bodyToMono(ReservaExternaDTO.class)
+                    .block();
 
             dto.setReserva(reservaRecuperada);
+
         } catch (Exception e) {
-            dto.setReserva(reserva:null);
+
+            dto.setReserva(null);
         }
+
         return dto;
     }
 }
