81d189ee10e899143e9c8168fd77ba445c10c8ba
FernandoNavarreteDuoc <fer.navarretea@duocuc.cl>
2026-06-22 18:06:04 -0400

    se agrega config a cliente

--- clientes/clientes/src/main/java/com/cliente/clientes/service/ClienteService.java
+++ clientes/clientes/src/main/java/com/cliente/clientes/service/ClienteService.java
@@ -24,7 +24,7 @@ public class ClienteService {
 
     @Autowired
     private WebClient.Builder webClientBuilder;
-
+    
     private static final Logger logger =
             LoggerFactory.getLogger(ClienteService.class);
 
