e76366ddb1d5b9034278372f8930b37c012a427d
FernandoNavarreteDuoc <fer.navarretea@duocuc.cl>
2026-06-26 18:38:00 -0400

    hateoas

--- reservas/reservas/src/main/java/com/reserva/reservas/assemblers/MetodopagoModelAssembler.java
+++ reservas/reservas/src/main/java/com/reserva/reservas/assemblers/MetodopagoModelAssembler.java
@@ -1,5 +1,22 @@
 package com.reserva.reservas.assemblers;
 
-public class MetodopagoModelAssembler {
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import com.reserva.reservas.controller.MetodopagoControllerV2;
+import com.reserva.reservas.model.Metodopago;
+
+@Component
+public class MetodopagoModelAssembler implements RepresentationModelAssembler<Metodopago, EntityModel<Metodopago>> {
+    @Override
+    public EntityModel<Metodopago> toModel (Metodopago metodopago){
+        return EntityModel.of(metodopago,
+            linkTo(methodOn(MetodopagoControllerV2.class).buscar(metodopago.getIdMetodoPago())).withSelfRel(),
+            linkTo(methodOn(MetodopagoControllerV2.class).listar()).withRel("metodospago"));
+    }
 
 }
--- reservas/reservas/src/main/java/com/reserva/reservas/assemblers/ReservaModelAssembler.java
+++ reservas/reservas/src/main/java/com/reserva/reservas/assemblers/ReservaModelAssembler.java
@@ -1,5 +1,22 @@
 package com.reserva.reservas.assemblers;
 
-public class ReservaModelAssembler {
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import com.reserva.reservas.controller.ReservaControllerV2;
+import com.reserva.reservas.model.Reserva;
+
+@Component
+public class ReservaModelAssembler implements RepresentationModelAssembler<Reserva, EntityModel<Reserva>> {
+    @Override
+    public EntityModel<Reserva> toModel (Reserva reserva){
+        return EntityModel.of(reserva,
+            linkTo(methodOn(ReservaControllerV2.class).buscar(reserva.getIdReserva())).withSelfRel(),
+            linkTo(methodOn(ReservaControllerV2.class).listar()).withRel("reservas"));
+    }
 
 }
