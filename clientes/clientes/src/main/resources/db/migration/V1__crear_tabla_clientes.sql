3895c79b0a48917fc329797be6bcf24f5e6f9866
maxiscript777 <max.amestica@duocuc.cl>
2026-06-28 18:05:08 -0400

    prueba 1

--- clientes/clientes/src/main/resources/db/migration/V1__crear_tabla_clientes.sql
+++ clientes/clientes/src/main/resources/db/migration/V1__crear_tabla_clientes.sql
@@ -1,20 +1,14 @@
 CREATE TABLE clientes (
-   nombre VARCHAR(50) NOT NULL,
-   direccion VARCHAR(100) NOT NULL,
-   telefono INTEGER NOT NULL,
-   correo VARCHAR(100) NOT NULL,
-   contraseña VARCHAR(20) NOT NULL
+    rut VARCHAR(12) NOT NULL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
+    telefono INTEGER NOT NULL,
+    correo VARCHAR(100) NOT NULL,
+    contraseña VARCHAR(20) NOT NULL
 );
 
-
-INSERT INTO clientes (nombre, direccion, telefono, correo, contraseña) VALUES ('Felipe Acosta', 'Bernardo Ohiggins 173', 123456789, 'felipe.acosta@example.com', 'password123');
-INSERT INTO clientes (nombre, direccion, telefono, correo, contraseña) VALUES ('Maria Gomez', 'Los Libertadores 426', 987654321, 'maria.gomez@example.com', 'c987654321');
-INSERT INTO clientes (nombre, direccion, telefono, correo, contraseña) VALUES ('Juan Perez', 'Trebulco 759', 849583745, 'juan.perez@example.com', 'pelusitatequiero123');
-INSERT INTO clientes (nombre, direccion, telefono, correo, contraseña) VALUES ('Ana Rodriguez', 'Juan Tenorio 123', 938465098, 'ana.rodriguez@example.com', 'soymuyfeliz6345');
-INSERT INTO clientes (nombre, direccion, telefono, correo, contraseña) VALUES ('Carlos Sanchez', 'Alcántara 426', 349805674, 'carlos.sanchez@example.com', 'arribalavida734');
-INSERT INTO clientes (nombre, direccion, telefono, correo, contraseña) VALUES ('Laura Fernandez', 'Los Libertadores 759', 583746920, 'laura.fernandez@example.com', 'lavidaesbella845');
-INSERT INTO clientes (nombre, direccion, telefono, correo, contraseña) VALUES ('Pedro Martinez', 'Trebulco 123', 748392056, 'pedro.martinez@example.com', 'pedromartinez123');
-INSERT INTO clientes (nombre, direccion, telefono, correo, contraseña) VALUES ('Sofia Lopez', 'Juan Tenorio 426', 938475620, 'sofia.lopez@example.com', 'sofiacarlos123');
-INSERT INTO clientes (nombre, direccion, telefono, correo, contraseña) VALUES ('Diego Ramirez', 'Alcántara 759', 583746920, 'diego.ramirez@example.com', '8473957864');
-
-
+INSERT INTO clientes (rut, nombre, direccion, telefono, correo, contraseña) VALUES ('12345678-9', 'Felipe Acosta', 'Bernardo Ohiggins 173', 123456789, 'felipe.acosta@example.com', 'password123');
+INSERT INTO clientes (rut, nombre, direccion, telefono, correo, contraseña) VALUES ('98765432-1', 'Maria Gomez', 'Los Libertadores 426', 987654321, 'maria.gomez@example.com', 'c987654321');
+INSERT INTO clientes (rut, nombre, direccion, telefono, correo, contraseña) VALUES ('11111111-1', 'Juan Perez', 'Trebulco 759', 849583745, 'juan.perez@example.com', 'pelusitatequiero123');
+INSERT INTO clientes (rut, nombre, direccion, telefono, correo, contraseña) VALUES ('22222222-2', 'Ana Rodriguez', 'Juan Tenorio 123', 938465098, 'ana.rodriguez@example.com', 'soymuyfeliz6345');
+INSERT INTO clientes (rut, nombre, direccion, telefono, correo, contraseña) VALUES ('33333333-3', 'Carlos Sanchez', 'Alcantara 426', 349805674, 'carlos.sanchez@example.com', 'arribalavida734');
