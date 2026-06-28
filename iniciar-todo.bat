7807ed0987a52e42e7b639318b9009a41f35a8ca
maxiscript777 <max.amestica@duocuc.cl>
2026-06-28 00:36:35 -0400

    bat

--- iniciar-todo.bat
+++ iniciar-todo.bat
@@ -1,30 +1,30 @@
 @echo off
 
 echo Iniciando Servidor de Descubrimiento Eureka (Puerto 8761)...
-cd eureka
+cd eureka\eureka
 start cmd /k "mvnw spring-boot:run"
 
-echo Esperando 12 segundos a que Eureka se estabilice...
-timeout /t 12 /nobreak > null
+echo Esperando 15 segundos a que Eureka se estabilice...
+timeout /t 15 /nobreak > nul
 
 echo Iniciando API Gateway...
-cd ../gateway
-start cmd /k "mvnw spring-boot:run"
-
-echo Iniciando Microservicio Clientes...
-cd ../clientes
-start cmd /k "mvnw spring-boot:run"
-
-echo Iniciando Microservicio Locales...
-cd ../locales
+cd ..\..\gateway\gateway
 start cmd /k "mvnw spring-boot:run"
 
 echo Iniciando Microservicio Reservas...
-cd ../reservas
+cd ..\..\reservas\reservas
+start cmd /k "mvnw spring-boot:run"
+
+echo Iniciando Microservicio Clientes...
+cd ..\..\clientes\clientes
+start cmd /k "mvnw spring-boot:run"
+
+echo Iniciando Microservicio Locales...
+cd ..\..\locales\locales
 start cmd /k "mvnw spring-boot:run"
 
 echo Iniciando Microservicio Ubicaciones...
-cd ../ubicaciones
+cd ..\..\ubicaciones\ubicaciones
 start cmd /k "mvnw spring-boot:run"
 
 echo Ecosistema lanzado. Dashboard disponible en http://localhost:8761
